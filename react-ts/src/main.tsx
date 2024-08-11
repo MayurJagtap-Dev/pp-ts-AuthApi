eaf9b30f2e5852f968259067e0a656cb2e75b0df
Mayur Jagtap <mayurjagtap9112@gmail.com>
2024-08-11 23:34:51 +0530

    state management

--- react-ts/src/main.tsx
+++ react-ts/src/main.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+//import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+import { store } from "./store";
+import { Provider } from "react-redux";
+//import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);

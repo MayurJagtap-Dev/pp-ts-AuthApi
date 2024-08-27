d96a43ecdf1e217824df7122f0d00a5dc3032cff
Mayur Jagtap <mayurjagtap9112@gmail.com>
2024-08-27 23:10:21 +0530

    Added ready event after new user joining to make calls

--- webrtc-video-calling/frontend/src/Context/SocketContext.tsx
+++ webrtc-video-calling/frontend/src/Context/SocketContext.tsx
@@ -1,8 +1,10 @@
 import SocketIoClient from "socket.io-client";
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useReducer } from "react";
 import { useNavigate } from "react-router-dom";
 import Peer from "peerjs";
 import { v4 as UUIDv4 } from "uuid";
+import { peerReducer } from "../Reducers/peerReducer";
+import { addPeerAction } from "../Actions/peerAction";
 const WS_Server = "http://localhost:5500";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -22,6 +24,8 @@ export const SocketProvider: React.FC<Props> = ({ children }) => {
 
   // state variable to store the userId
   const [user, setUser] = useState<Peer>(); // new peer user
+  const [stream, setStream] = useState<MediaStream>();
+  const [peers, dispatch] = useReducer(peerReducer, {}); // peers->state
 
   const fetchParticipantList = ({
     roomId,
@@ -34,10 +38,24 @@ export const SocketProvider: React.FC<Props> = ({ children }) => {
     console.log(roomId, participants);
   };
 
+  const fetchUserFeed = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    setStream(stream);
+  };
+
   useEffect(() => {
     const userId = UUIDv4();
-    const newPeer = new Peer(userId);
+    const newPeer = new Peer(userId, {
+      host: "localhost",
+      port: 9000,
+      path: "/myapp",
+    });
     setUser(newPeer);
+    fetchUserFeed();
+
     const enterRoom = ({ roomId }: { roomId: string }) => {
       navigate(`/room/${roomId}`);
     };
@@ -47,8 +65,31 @@ export const SocketProvider: React.FC<Props> = ({ children }) => {
     socket.on("get-users", fetchParticipantList);
   }, []);
 
+  useEffect(() => {
+    if (!user || !stream) return;
+
+    socket.on("user-joined", ({ peerId }) => {
+      const call = user.call(peerId, stream);
+      console.log("Calling the new peer", peerId);
+      call.on("stream", () => {
+        dispatch(addPeerAction(peerId, stream));
+      });
+    });
+
+    user.on("call", (call) => {
+      // what to do when other peers on the group call you when u joined
+      console.log("receiving a call");
+      call.answer(stream);
+      call.on("stream", () => {
+        dispatch(addPeerAction(call.peer, stream));
+      });
+    });
+
+    socket.emit("ready");
+  }, [user, stream]);
+
   return (
-    <SocketContext.Provider value={{ socket, user }}>
+    <SocketContext.Provider value={{ socket, user, stream, peers }}>
       {children}
     </SocketContext.Provider>
   );
--- webrtc-video-calling/frontend/src/Pages/Room.tsx
+++ webrtc-video-calling/frontend/src/Pages/Room.tsx
@@ -1,10 +1,11 @@
 import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { SocketContext } from "../Context/SocketContext";
+import UserFeedPlayer from "../Components/UserFeedPlayer";
 
 const Room: React.FC = () => {
   const { id } = useParams();
-  const { socket, user } = useContext(SocketContext);
+  const { socket, user, stream, peers } = useContext(SocketContext);
 
   useEffect(() => {
     // emitting this event so that either creator of room or joinee in the room
@@ -16,7 +17,22 @@ const Room: React.FC = () => {
     }
   }, [id, user, socket]);
 
-  return <div>room : {id}</div>;
+  return (
+    <div>
+      room : {id}
+      <br />
+      Your own user feed
+      <UserFeedPlayer stream={stream} />
+      <div>
+        Other Users feed
+        {Object.keys(peers).map((peerId) => (
+          <>
+            <UserFeedPlayer key={peerId} stream={peers[peerId].stream} />
+          </>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Room;
